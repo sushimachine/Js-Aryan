6d00e801a63f166c39742c817394adea0bf086d2
sushimachine <aryan456.rehla@gmail.com>
2025-07-25 15:40:05 +0000

    yep

---
+++ 02_DataTypes/test.js
@@ -0,0 +1,25 @@
+"use strict" //treat all JS code as a newer version
+
+//alert(3+3) //we are using node js, not browser
+
+console.log(3+3)
+console.log("Aryan")
+
+let name = "Aryan"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to the power 53
+//bigInt
+//string => ""
+// boolean =>true/false
+// null => Standalone value 
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null)  //object
+console.log(typeof isLoggedIn)
